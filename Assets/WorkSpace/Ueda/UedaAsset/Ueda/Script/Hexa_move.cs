414330f69d26d3d0944a4abe68a153702be56d1e
Uedaaaaaa <88474724+Uedaaaaaa@users.noreply.github.com>
2021-10-16 02:33:49 +0900

    a

---
+++ Assets/WorkSpace/Ueda/UedaAsset/Ueda/Script/Hexa_move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexa_move : MonoBehaviour
+{
+    float time;
+    void Start()
+    {
+        time = 0;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        float z = Mathf.PerlinNoise(time, 0);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, z);
+    }
+}
