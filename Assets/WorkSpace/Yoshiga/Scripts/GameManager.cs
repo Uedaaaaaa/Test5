6fdf0082135cfe2bf30f23fc99beb964e40aa201
leontaicho <92150483+leontaicho@users.noreply.github.com>
2021-10-15 23:06:58 +0900

    iroiro

--- Assets/WorkSpace/Yoshiga/Scripts/GameManager.cs
+++ Assets/WorkSpace/Yoshiga/Scripts/GameManager.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//キャラクタークラス
+public class Character
+{
+    //キャラクター本体
+    private GameObject MyObj;
+    //キャラクターのRigidbody
+    private Rigidbody MyRB;
+    //キャラクターが持っている飴の個数
+    public int Candy;
+    //キャラクターのやる気
+    public int Yaruki;
+
+    //キャラクターの初期値設定の関数
+    public Character()
+    {
+        this.Candy = 0;
+        this.Yaruki = 5;
+    }
+}
+
 public class GameManager : MonoBehaviour
 {
+    //キャラクター配列
+    [HideInInspector]
+    public Character[] characters = new Character[4];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0;i < characters.Length; ++i)
+        {
+            characters[i].
+        }
     }
 
     // Update is called once per frame
