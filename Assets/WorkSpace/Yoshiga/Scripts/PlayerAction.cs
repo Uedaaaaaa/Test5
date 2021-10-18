53c5be761adb8a7980097b3fc9c2dbec5cfba3e4
leontaicho <92150483+leontaicho@users.noreply.github.com>
2021-10-18 22:58:24 +0900

    aaa

---
+++ Assets/WorkSpace/Yoshiga/Scripts/PlayerAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    //ゲームマネージャー
+    private GameManager manager;
+    //自身のナンバー
+    private int MyNo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GameManager>();
+        MyNo = int.Parse(gameObject.name.Substring(0, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
