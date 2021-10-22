de7ad469b9a909a357ba4e03c5c5f7ad5b528f32
KimuraHiromu <92150610+KimuraHiromu@users.noreply.github.com>
2021-10-22 18:19:38 +0900

    kimura

---
+++ Assets/WorkSpace/Kimura/UI/Scripts/DiceNumUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DiceNumUI : MonoBehaviour
+{
+    public Image MyImage;
+    public Sprite[] Num = new Sprite[6];
+    int sampleNum = 0;
+    bool nullflg = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(!nullflg)
+        {
+            sampleNum++;
+            if (sampleNum == 6)
+            {
+                sampleNum = 0;
+            }
+            MyImage.sprite = Num[sampleNum];
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            nullflg = true;
+        }
+    }
+}
---
+++ Assets/WorkSpace/Kimura/UI/Scripts/SampleUISet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SampleUISet : MonoBehaviour
+{
+    public Image[] PlayerTurnUI = new Image[4];
+    Image SetPlayerTurnUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < PlayerTurnUI.Length;i++)
+        {
+
+        }
+    }
+    void PlayerTrunUIDestroy()
+    {
+        if (SetPlayerTurnUI != null)
+        {
+            Destroy(SetPlayerTurnUI);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            SetPlayerTurnUI = Instantiate(PlayerTurnUI[0]);
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            SetPlayerTurnUI = Instantiate(PlayerTurnUI[1]);
+        }
+    }
+}
