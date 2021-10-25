a89f2edaad0ae238be8588242e185f657ba22393
KimuraHiromu <92150610+KimuraHiromu@users.noreply.github.com>
2021-10-25 12:18:17 +0900

    kimura

--- Assets/WorkSpace/Kimura/UI/Scripts/SampleUISet.cs
+++ Assets/WorkSpace/Kimura/UI/Scripts/SampleUISet.cs
@@ -4,33 +4,72 @@ using UnityEngine;
 using UnityEngine.UI;
 public class SampleUISet : MonoBehaviour
 {
-    public Image[] PlayerTurnUI = new Image[4];
-    GameObject camera;
+    //実際に表示するプレイヤーターンUI
+    [SerializeField] Image ImagePlayerTurnUI;
+    [Header("プレイヤーのターンUIの移動速度")]
+    [SerializeField] float TurnUIMoveSpeed = 1.0f;
+
+    //プレイヤーターンUIのスプライト獲得
+    [SerializeField] Image[] PlayerTurnUI = new Image[4];
+
+    //プレイヤーターンUIの表示フラグ
+    //trueなら表示中
+    private bool PlayerTurnUIMoveFlg = false;
+
+    //実際に表示するダイスUI
+    [SerializeField] Image DiceImage;
+
+    //ダイスUIのスプライト
+    [SerializeField] Sprite[] DiceNumSprite = new Sprite[6];
+    [Header("ダイスUIの位置")]
+    [SerializeField] Vector3 DiceImagePos = new Vector3(0,4,0);
     // Start is called before the first frame update
     void Start()
     {
-        camera = GameObject.FindGameObjectWithTag("MainCamera");
-        for (int i = 0; i < PlayerTurnUI.Length -1;i++)
+        //UIの表示をオフにする
+        ImagePlayerTurnUI.gameObject.SetActive(false);
+        DiceImage.gameObject.SetActive(false);
+    }
+    public void PlayerTurnUISet(int PlayerNum)
+    {
+        if (!PlayerTurnUIMoveFlg)
         {
-            PlayerTurnUI[i].enabled = false;
+            ImagePlayerTurnUI.gameObject.SetActive(true);
+            ImagePlayerTurnUI.sprite = PlayerTurnUI[PlayerNum].sprite;
+            PlayerTurnUIMoveFlg = true;
+            ImagePlayerTurnUI.transform.position += new Vector3(22, 0, 0);
         }
     }
-    public void SetPlayerTurnUI(int PlayerNum)
+    public void DiceUISet(int DiceNum)
     {
-        PlayerTurnUI[PlayerNum].enabled = true;
-        PlayerTurnUI[PlayerNum].gameObject.transform.position = (camera.transform.position);
-        PlayerTurnUI[PlayerNum].gameObject.transform.position += new Vector3(10,0,0); 
+        DiceImage.transform.position = this.gameObject.transform.position + DiceImagePos;
+        DiceImage.gameObject.SetActive(true);
+        DiceImage.sprite = DiceNumSprite[DiceNum - 1];
+    }
+    public void DiceUIDestroy()
+    {
+        DiceImage.gameObject.SetActive(false);
     }
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.A))
         {
-            SetPlayerTurnUI(0);
+            PlayerTurnUISet(Random.Range(0,4));
         }
         if(Input.GetKeyDown(KeyCode.B))
         {
-
+            DiceUISet(3);
+        }
+        if(PlayerTurnUIMoveFlg)
+        {
+            ImagePlayerTurnUI.transform.position -= new Vector3(0.1f * TurnUIMoveSpeed, 0, 0);
+            if(ImagePlayerTurnUI.transform.position.x <= this.gameObject.transform.position.x - 22.0f)
+            {
+                PlayerTurnUIMoveFlg = false;
+                ImagePlayerTurnUI.transform.position = this.gameObject.transform.position;
+                ImagePlayerTurnUI.gameObject.SetActive(false);
+            }
         }
     }
 }
