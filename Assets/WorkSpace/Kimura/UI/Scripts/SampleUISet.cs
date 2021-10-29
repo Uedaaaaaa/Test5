952961c02b0c3fef461b2cde364bd2e66e3b301a
KimuraHiromu <92150610+KimuraHiromu@users.noreply.github.com>
2021-10-29 17:17:28 +0900

    kimura

--- Assets/WorkSpace/Kimura/UI/Scripts/SampleUISet.cs
+++ Assets/WorkSpace/Kimura/UI/Scripts/SampleUISet.cs
@@ -4,114 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 public class SampleUISet : MonoBehaviour
 {
-    //実際に表示するプレイヤーターンUI
-    [SerializeField] Image ImagePlayerTurnUI;
-    [Header("プレイヤーのターンUIの移動速度")]
-    [SerializeField] float TurnUIMoveSpeed = 1.0f;
-
-    //プレイヤーターンUIのスプライト獲得
-    [SerializeField] Image[] PlayerTurnUI = new Image[4];
-
-    //プレイヤーターンUI表示と同時にセットするBボタンUI
-    [SerializeField] Image PlayerTurnBbuttonUI;
-
-    //ダイスを止めると書かれたUI
-    [SerializeField] Image DiceStopUI;
-
-    //実際に表示するダイスの数字UI
-    [SerializeField] Image DiceImage;
-
-    //ダイスの数字UIのスプライト
-    [SerializeField] Sprite[] DiceNumSprite = new Sprite[6];
-    [Header("ダイスUIの位置")]
-    [SerializeField] Vector3 DiceImagePos = new Vector3(0,4,0);
-
-    //プレイヤーのステータスUI
-    [SerializeField] Image[] PlayerStatusUI = new Image[4];
-    //プレイヤーのやる気、キャンディの数
-    [SerializeField] Text[] Candytxt = new Text[4];
-    [SerializeField] Text[] Yarukitxt = new Text[4];
-
-    //ゲームマネージャー
-    GameManager manager;
+    SetCharacerUI sample;
+    [SerializeField] GameObject UIsample;
     // Start is called before the first frame update
     void Start()
     {
-        //UIの表示をオフにする
-        ImagePlayerTurnUI.gameObject.SetActive(false);
-        PlayerTurnBbuttonUI.gameObject.SetActive(false);
-        DiceStopUI.gameObject.SetActive(false);
-        DiceImage.gameObject.SetActive(false);
-        for(int i = 0; i < PlayerStatusUI.Length; i++)
-        {
-            PlayerStatusUI[i].gameObject.SetActive(false);
-            Candytxt[i].text = "";
-            Yarukitxt[i].text = "";
-        }
-
-        //ゲームマネージャーを獲得
-        manager = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GameManager>();
-        //プレイヤーのキャンディ、やる気を獲得する
-        for(int i = 0; i < manager.characters.Length; i++)
-        {
-            PlayerStatusUI[i].gameObject.SetActive(true);
-            Candytxt[i].text = manager.characters[i].Candy.ToString().PadLeft(2,'0');
-            Yarukitxt[i].text = manager.characters[i].Yaruki.ToString().PadLeft(2,'0');
-        }
+        sample = UIsample.GetComponent<SetCharacerUI>();
     }
-
-    //ターンUIの表示非表示
-    public void PlayerTurnUISet(int PlayerNum)
+    void SetUI(int a,bool flg)
     {
-        ImagePlayerTurnUI.gameObject.SetActive(true);
-        ImagePlayerTurnUI.sprite = PlayerTurnUI[PlayerNum].sprite;
-
-        PlayerTurnBbuttonUI.gameObject.SetActive(true);
+        Debug.Log("int,bool");
     }
-    public void PlayerTurnUIDestroy()
+    void SetUI(bool flg)
     {
-        ImagePlayerTurnUI.gameObject.SetActive(false);
-        PlayerTurnBbuttonUI.gameObject.SetActive(false);
+        Debug.Log("bool");
     }
-
-    //ダイスを止めるUIの表示非表示
-    public void DiceStopUISet()
-    {
-        DiceStopUI.gameObject.SetActive(true);
-    }
-    public void DiceStopUIDestroy()
-    {
-        DiceStopUI.gameObject.SetActive(false);
-    }
-
-    //ダイスの数のUIの表示非表示
-    public void DiceNumUISet(int DiceNum)
-    {
-        DiceImage.transform.position = this.gameObject.transform.position + DiceImagePos;
-        DiceImage.gameObject.SetActive(true);
-        DiceImage.sprite = DiceNumSprite[DiceNum - 1];
-    }
-    public void DiceNumUIDestroy()
-    {
-        DiceImage.gameObject.SetActive(false);
-    }
-
-    //プレイヤーのステータスUIの更新
-    public void PlayerStatusChange()
-    {
-        for (int i = 0; i < manager.characters.Length - 1; i++)
-        {
-            Candytxt[i].text = manager.characters[i].Candy.ToString();
-            Yarukitxt[i].text = manager.characters[i].Yaruki.ToString();
-        }
-    }
-
-
-
-
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKey(KeyCode.A))
+        {
+            sample.DiceStopUISet();
+            SetUI(1,false);
+        }
+        else
+        {
+            sample.DiceStopUIDestroy();
+        }
+        if(Input.GetKey(KeyCode.B))
+        {
+            SetUI(false);
+            sample.DiceNumUISet(3);
+        }
+        else
+        {
+            sample.DiceNumUIDestroy();
+        }
     }
 }
